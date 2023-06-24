d62d6ce7583d17f10992ad4e543760d723c54039
marcelovesantera <marcelorv.dm@gmail.com>
2023-06-24 18:37:09 -0300

    Initial ProjectsController

---
+++ motionfreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using motionfreela.API.Models;
+
+namespace motionfreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProjects()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id")]
+        public IActionResult GetProjectById(int id)
+        {
+            // return NotFound();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProject([FromBody] CreateProjectModel createProject)
+        {
+            // return BadRequest();
+
+            return CreatedAtAction(nameof(GetProjectById), new { id = createProject.Id }, createProject);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProject(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            // return BadRequest();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProject(int id)
+        {
+            // return NotFound();
+
+            return NoContent();
+        }
+    }
+}
