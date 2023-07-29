d68efaa3007287cc1d39a6a61e2e64c93c9d0f68
marcelovesantera <marcelorv.dm@gmail.com>
2023-07-29 19:52:39 -0300

    Deleting unused 'Usings"

--- motionfreela.Core/Exceptions/ProjectAlreadyStartedException.cs
+++ motionfreela.Core/Exceptions/ProjectAlreadyStartedException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace motionfreela.Core.Exceptions
+﻿namespace motionfreela.Core.Exceptions
 {
     public class ProjectAlreadyStartedException : Exception
     {
