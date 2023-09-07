5e3a5314cb2bf4f73a6219cb930377ff83786e3d
Marcelo Vesantera <mrvesantera@hotmail.com>
2023-09-07 14:41:10 -0300

    Project interface and implementation service

---
+++ motionfreela.Application/Services/Implementations/ProjectService.cs
@@ -0,0 +1,49 @@
+﻿using motionfreela.Application.InputModels;
+using motionfreela.Application.Services.Interfaces;
+using motionfreela.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace motionfreela.Application.Services.Implementations
+{
+    public class ProjectService : IProjectService
+    {
+        public int CreateProject(CreateProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProject(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FinishProject(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProjectDetailsViewModel GetProjectById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProjectViewModel> GetProjects(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartProject(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProject(UpdateProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
